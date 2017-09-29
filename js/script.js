b395c6e81d2fe11343227a2551bbf3c97be5ebb8
dgtlslave <slavko.alexandr@gmail.com>
2017-09-29 17:39:55 +0300

    slider overflow todo

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $('.work__slider').slick({
+  accessibility: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  centerPadding: '10px',
+  variableWidth: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+  });
+
+});
